9e44feee5240337dbf8ab5a9170d8d1edb4b060e
sbx_user1060 <sbx_user1060@ip-10-129-161-67.eu-west-2.compute.internal>
2018-11-25 11:42:46 +0000

    Sun Nov 25 11:42:46 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -12,27 +12,73 @@ public class CheckoutSolution {
         }
 
         Map <String,Integer> item = new HashMap();
+        Map <String,Integer> matchMap = new HashMap();
 
         item.put("A",50);
         item.put("B",30);
         item.put("C",20);
         item.put("D",15);
         String[] list =skus.split("");
+
         if(list.length==1){
             if(item.containsKey(skus)) {
                 return item.get(skus);
             }else
                 return -1;
         }
+
+        for (String match:list
+             ) {
+            if(matchMap.containsKey(match)){
+                matchMap.replace(match,matchMap.get(match),matchMap.get(match+1));
+            }
+            else {
+                if(item.containsKey(match)) {
+                    matchMap.put(match, 1);
+                }else{
+                    return  -1;
+                }
+            }
+        }
+
+
         int sum=0;
-        for (String val:list
+        int specialA=130;
+        int specialB=45;
+        for (Map.Entry<String,Integer> entry:matchMap.entrySet()
+             ) {
+            if(entry.getKey().equals("A")){
+                if(entry.getValue()==3){
+                    sum = sum+specialA;
+                }else if(entry.getValue()>3){
+                    int round = Math.round(entry.getValue()/3);
+                    sum = sum+round*specialA;
+                    int bal = entry.getValue()%3;
+                    sum= sum+item.get("A");
+
+                }
+            }if(entry.getKey().equals("B")){
+                if(entry.getValue()==2){
+                    sum = sum+specialB;
+                }else if(entry.getValue()>2){
+                    int round = Math.round(entry.getValue()/3);
+                    sum = sum+round*specialB;
+                    int bal = entry.getValue()%3;
+                    sum= sum+item.get("A");
+
+                }
+            }
+            sum=sum+item.get(entry.getKey())*entry.getValue();
+        }
+        /*for (String val:list
              ) {
             if(item.containsKey(val)){
                 sum= sum+item.get(val);
             }else{
                 sum= -1;
+                break;
             }
-        }
+        }*/
 
         return sum;
     }
