01026479548611f20def8164ec8b552d0049151d
sbx_user1060 <sbx_user1060@ip-10-129-161-67.eu-west-2.compute.internal>
2018-11-25 11:06:10 +0000

    Sun Nov 25 11:06:10 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,9 +2,29 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-
+        if(skus==null){
+            return -1;
+        }
+        Map <String,Integer> item = new HashMap();
+        item.put("A",50);
+        item.put("B",30);
+        item.put("C",20);
+        item.put("D",15);
+        String[] list =skus.split(",");
+        for (String val:list
+             ) {
+            if(item.containsKey(val)){
+                return -1;
+            }
+        }
+        if(list.length==1){
+            return item.get(skus);
+        }
         return -1;
     }
 }
